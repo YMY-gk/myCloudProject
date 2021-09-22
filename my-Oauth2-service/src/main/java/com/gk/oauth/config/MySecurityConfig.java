fd5c604820c8120dec32dcc3d5710aab81d66b23
guokui <15637521598@163.com>
2021-09-22 23:48:46 +0800

    zxxa

---
+++ my-Oauth2-service/src/main/java/com/gk/oauth/config/MySecurityConfig.java
@@ -0,0 +1,49 @@
+package com.gk.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author guokui
+ * @class security
+ * @date 2021/9/2 16:47
+ */
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();//没有加密
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().and()
+                .csrf().disable()
+                .httpBasic();
+    }
+
+}
---
+++ my-Oauth2-service/src/main/java/com/gk/oauth/service/MyUserDetialService.java
@@ -0,0 +1,34 @@
+package com.gk.oauth.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author guokui
+ * @class security
+ * @date 2021/9/2 17:06
+ */
+@Service("UserDetailsService")
+@Slf4j
+public class MyUserDetialService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 判断用户名是否存在
+        if (!"user".equals(username)){
+            throw new UsernameNotFoundException("用户名不存在！");
+        }
+        log.error("-----------------------------loadUserByUsername-----------------------------------");
+        // 从数据库中获取的密码 atguigu 的密文
+        String pwd ="user";
+        // 第三个参数表示权限
+        return new User(username,new BCryptPasswordEncoder().encode(pwd), AuthorityUtils.commaSeparatedStringToAuthorityList("user,ROLE_role1"));
+    }
+}
